945a6e01c93b2c820898d7684382d3d3571a8adf
Siddartha Kunwar <siddartha.kunwar01@gmail.com>
2026-02-06 17:15:27 +0545

    feat: add login, signup and logout functionality

--- linkup_project/accounts/views.py
+++ linkup_project/accounts/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import LoginForm, SignUpForm
 
-# Create your views here.
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save() 
+            login(request, user)  
+            return redirect('core:feed')
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(request, email=email, password=password)
+            if user:
+                login(request, user)
+                return redirect('core:feed')
+            else:
+                form.add_error(None, 'Invalid email or password')
+    else:
+        form = LoginForm()
+
+    return render(request, 'accounts/login.html', {'form': form})
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('accounts:login')
