68cdd5749885a47e93ee6e2a48ea68a274b16fea
Siddartha Kunwar <siddartha.kunwar01@gmail.com>
2026-02-06 17:29:16 +0545

    feat: add path for apps at the project level and updated settings for folder location

--- linkup_project/linkup_project/urls.py
+++ linkup_project/linkup_project/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
+    path('posts/', include('posts.urls', namespace='posts')),
+    path('follows/',include('follows.urls', namespace='follows')),
+    path('profiles/', include('profiles.urls', namespace='profiles')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
