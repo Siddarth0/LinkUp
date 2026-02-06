38bd58ca021c7d742520f7bd917a3dfadb8ddaeb
Siddartha Kunwar <siddartha.kunwar01@gmail.com>
2026-02-06 19:48:58 +0545

    feat: add Tailwind CSS styling to signup and login form fields

--- linkup_project/accounts/forms.py
+++ linkup_project/accounts/forms.py
@@ -9,6 +9,20 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ('email', 'username', 'password1', 'password2')
 
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': 'w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500'
+            })
+
 class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': 'w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500'
+            })
