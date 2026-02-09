d9be85586872e801e5dcbc62d45838546a829daf
Siddartha Kunwar <siddartha.kunwar01@gmail.com>
2026-02-09 13:22:40 +0545

    feat: implement ajax for infinte scrolling

--- linkup_project/core/views.py
+++ linkup_project/core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from posts.models import Post
+from django.http import JsonResponse
+from django.template.loader import render_to_string
 
 @login_required
 def feed_view(request):
@@ -21,5 +23,16 @@ def feed_view(request):
     paginator = Paginator(posts_list, 10)
     page_number = request.GET.get('page')
     posts = paginator.get_page(page_number)
+
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        html = render_to_string(
+            'posts/post_list.html',
+            {'posts': posts},
+            request=request
+        )
+        return JsonResponse({
+            'html': html,
+            'has_next': posts.has_next()
+        })
 
     return render(request, 'core/feed.html', {'posts': posts})
