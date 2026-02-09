030a960610bd144966eb3a988cf3a0157f79ed3b
Siddartha Kunwar <siddartha.kunwar01@gmail.com>
2026-02-09 12:44:31 +0545

    feat: add notification logic

--- linkup_project/notifications/views.py
+++ linkup_project/notifications/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Notification
 
-# Create your views here.
+@login_required
+def notification_view(request):
+    notifications = Notification.objects.filter(receiver=request.user).select_related('sender','post')
+
+    notifications.filter(is_read=False).update(is_read=True)
+    
+    return render(request, 'notifications/notification_list.html', {'notifications': notifications})
